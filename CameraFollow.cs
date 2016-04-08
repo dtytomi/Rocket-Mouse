b1cd38781d5036738f02385c8712a40edcf69e7e
dtytomi <dtytomi@gmail.com>
2016-04-08 16:21:59 +0100

    first commit

---
+++ CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿	using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject targetObjet;
+
+	private float distanceToTarget;
+
+	// Use this for initialization
+	void Start () {
+		distanceToTarget = transform.position.x - targetObjet.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float targetObjetX = targetObjet.transform.position.x;
+
+		Vector3 newCameraPosition = transform.position;
+		newCameraPosition.x = targetObjetX + distanceToTarget;
+		transform.position = newCameraPosition;
+	}
+}
